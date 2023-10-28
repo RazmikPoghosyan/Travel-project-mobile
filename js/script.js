7c4185f04d9299e4281a5e9850ecf61c5b5e1705
RazmikPoghosyan <razpoghosyaan@gmail.com>
2023-10-28 18:37:50 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+    center: true,
+    loop:true,
+    margin: 30,
+    startPosition: 1,
+    items:1,
+    responsive : {
+
+        750: {
+            items : 3,
+        },
+        850: {
+            startPosition:1,
+            items: 3,
+        },
+        1200: {
+            margin :30,
+        },
+    }
+});
+
+$('.slider__btn--prev').click(function() {
+    owl.trigger('prev.owl.carousel');
+})
+
+$('.slider__btn--next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+
+// nav icon
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav');
+const menuIcon = document.querySelector('.menu-icon');
+
+navBtn.onclick = function(){
+    nav.classList.toggle('nav--mobiel')
+    menuIcon.classList.toggle('menu-icon-active');
+    document.body.classList.toggle('no-scroll');
+}
+
+
